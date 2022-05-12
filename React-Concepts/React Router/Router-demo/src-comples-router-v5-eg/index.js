1e8491820102a9c2b648dad4733e1d8a2ef78834
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-12 20:05:01 +0530

    edits

--- React-Concepts/React Router/Router-demo/src-comples-router-v5-eg/index.js
+++ React-Concepts/React Router/Router-demo/src-comples-router-v5-eg/index.js
@@ -1,10 +1,15 @@
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import * as  ReactDOMClient from 'react-dom/client';
+
 
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(
     <BrowserRouter>
         <App />
-    </BrowserRouter>, document.getElementById('root'));
+    </BrowserRouter>);
